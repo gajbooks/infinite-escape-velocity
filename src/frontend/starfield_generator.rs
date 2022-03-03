6e07fcefa803a968558e5a17811012689a03f0c6
Grant Jennings <grant.jennings@codescientists.com>
2022-03-03 16:05:34 -0600

    Add basic color to stars in starfield

--- src/frontend/starfield_generator.rs
+++ src/frontend/starfield_generator.rs
@@ -15,10 +15,39 @@
     along with Infinite Escape Velocity.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use macroquad::prelude::*;
 use fxhash::*;
+use macroquad::prelude::*;
+use std::hash::{Hash, Hasher};
+
+const STAR_TRANSPARENCY: u8 = 200;
+
+const SPECTRA_TABLE: [Color; 25] = [
+    color_u8!(255, 177, 14, STAR_TRANSPARENCY),
+    color_u8!(255, 192, 44, STAR_TRANSPARENCY),
+    color_u8!(255, 204, 76, STAR_TRANSPARENCY),
+    color_u8!(255, 213, 105, STAR_TRANSPARENCY),
+    color_u8!(255, 219, 131, STAR_TRANSPARENCY),
+    color_u8!(255, 224, 153, STAR_TRANSPARENCY),
+    color_u8!(255, 227, 171, STAR_TRANSPARENCY),
+    color_u8!(255, 229, 187, STAR_TRANSPARENCY),
+    color_u8!(255, 230, 200, STAR_TRANSPARENCY),
+    color_u8!(255, 231, 211, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 221, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 229, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 236, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 243, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 248, STAR_TRANSPARENCY),
+    color_u8!(255, 232, 253, STAR_TRANSPARENCY),
+    color_u8!(252, 230, 255, STAR_TRANSPARENCY),
+    color_u8!(248, 226, 255, STAR_TRANSPARENCY),
+    color_u8!(245, 223, 255, STAR_TRANSPARENCY),
+    color_u8!(242, 221, 255, STAR_TRANSPARENCY),
+    color_u8!(240, 218, 255, STAR_TRANSPARENCY),
+    color_u8!(238, 216, 255, STAR_TRANSPARENCY),
+    color_u8!(236, 214, 255, STAR_TRANSPARENCY),
+    color_u8!(234, 213, 255, STAR_TRANSPARENCY),
+    color_u8!(232, 211, 255, STAR_TRANSPARENCY),
+];
 
 pub struct StarfieldGenerator {
     max_coordinate_divisor: f32,
@@ -26,7 +55,7 @@ pub struct StarfieldGenerator {
     world_coordinates_per_cell: f32,
     min_size: f32,
     max_size: f32,
-    star_list: Vec<StarData>
+    star_list: Vec<StarData>,
 }
 
 struct StarData {
@@ -34,18 +63,25 @@ struct StarData {
     screen_y: f32,
     speed_multiplier: f32,
     spectra: f32,
-    size: f32
+    size: f32,
 }
 
 impl StarfieldGenerator {
-    pub fn new(max_coordinate_divisor: f32, min_coordinate_divisor: f32, world_coordinates_per_cell: f32, min_size: f32, max_size: f32) -> StarfieldGenerator {
-
-        StarfieldGenerator{max_coordinate_divisor: max_coordinate_divisor,
+    pub fn new(
+        max_coordinate_divisor: f32,
+        min_coordinate_divisor: f32,
+        world_coordinates_per_cell: f32,
+        min_size: f32,
+        max_size: f32,
+    ) -> StarfieldGenerator {
+        StarfieldGenerator {
+            max_coordinate_divisor: max_coordinate_divisor,
             min_coordinate_divisor: min_coordinate_divisor,
             world_coordinates_per_cell: world_coordinates_per_cell,
             min_size: min_size,
             max_size: max_size,
-            star_list: Vec::new()}
+            star_list: Vec::new(),
+        }
     }
 
     fn simple_xorshift(state: u64) -> u64 {
@@ -57,7 +93,7 @@ impl StarfieldGenerator {
     }
 
     fn zero_one_generator(random: u64) -> f32 {
-        return  random as f32 / u64::MAX as f32;
+        return random as f32 / u64::MAX as f32;
     }
 
     fn star_from_coordinates(&self, x: i64, y: i64) -> StarData {
@@ -79,15 +115,22 @@ impl StarfieldGenerator {
         let seed = Self::simple_xorshift(seed);
         let size = Self::zero_one_generator(seed);
 
-        return StarData{
+        return StarData {
             screen_x: (x as f32 - 1.0 + x_offset) * self.world_coordinates_per_cell,
             screen_y: (y as f32 - 1.0 + y_offset) * self.world_coordinates_per_cell,
             speed_multiplier: speed_multiplier,
             spectra: spectra,
-            size: size};
+            size: size
+        };
     }
 
-    pub fn draw_stars(&mut self, x_coordinate: f32, y_coordinate: f32, screen_width: f32, screen_height: f32) {
+    pub fn draw_stars(
+        &mut self,
+        x_coordinate: f32,
+        y_coordinate: f32,
+        screen_width: f32,
+        screen_height: f32,
+    ) {
         let x_coordinate = x_coordinate / self.max_coordinate_divisor;
         let y_coordinate = y_coordinate / self.max_coordinate_divisor;
 
@@ -97,7 +140,12 @@ impl StarfieldGenerator {
         let cells_x = (screen_width / self.world_coordinates_per_cell) + 1.0;
         let cells_y = (screen_height / self.world_coordinates_per_cell) + 1.0;
 
-        self.generate_stars(start_x as i64, start_y as i64, cells_x as i64, cells_y as i64);
+        self.generate_stars(
+            start_x as i64,
+            start_y as i64,
+            cells_x as i64,
+            cells_y as i64,
+        );
 
         let divisor_ratio = (self.max_coordinate_divisor / self.min_coordinate_divisor) - 1.0;
 
@@ -113,10 +161,14 @@ impl StarfieldGenerator {
             let star_size = (star.size * size_range) + self.min_size;
             let star_x = (star.screen_x - x_coordinate) * offset_ratio;
             let star_y = (star.screen_y - y_coordinate) * offset_ratio;
-            
-            if star_x > min_x_to_draw && star_y > min_y_to_draw && star_x < max_x_to_draw && star_y < max_y_to_draw
+
+            if star_x > min_x_to_draw
+                && star_y > min_y_to_draw
+                && star_x < max_x_to_draw
+                && star_y < max_y_to_draw
             {
-                draw_circle(star_x, star_y, star_size, WHITE);
+                let spectra_lookup = (star.spectra * SPECTRA_TABLE.len() as f32) as usize;
+                draw_circle(star_x, star_y, star_size, SPECTRA_TABLE[spectra_lookup]);
             }
         }
     }
@@ -126,7 +178,8 @@ impl StarfieldGenerator {
 
         for i in 0..cells_y as i64 {
             for j in 0..cells_x as i64 {
-                self.star_list.push(self.star_from_coordinates(j + start_x as i64, i + start_y as i64));
+                self.star_list
+                    .push(self.star_from_coordinates(j + start_x as i64, i + start_y as i64));
             }
         }
     }
---
+++ src/misc/blackbody_table_generator.rs
@@ -0,0 +1,121 @@
+/*
+    This file is part of Infinite Escape Velocity.
+
+    Infinite Escape Velocity is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Infinite Escape Velocity is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Infinite Escape Velocity.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+type ColorTriple = (f64, f64, f64);
+
+fn main() {
+
+    for temp in (1000..12000).step_by(440)
+    {
+        let color = color_at_temperature(temp as f64);
+
+        let r = (color.0 * 255.0) as i32;
+        let g = (color.1 * 255.0) as i32;
+        let b = (color.2 * 255.0) as i32;
+    
+        println!("vec3({r}, {g}, {b}),");
+    }
+
+}
+
+fn color_at_temperature(temperature: f64) -> ColorTriple {
+    let CIE = integrate_CIE_tristimulus(temperature);
+    let sRGB = CIE_to_sRGB(CIE);
+
+    let normalize = 1.0 / sRGB.0.max(sRGB.1.max(sRGB.2));
+    return (sRGB.0 * normalize, sRGB.1 * normalize, sRGB.2 * normalize);
+}
+
+fn integrate_CIE_tristimulus(temperature: f64) -> ColorTriple {
+    let mut X: f64 = 0.0;
+    let mut Y: f64 = 0.0;
+    let mut Z: f64 = 0.0;
+
+    for wavelength in 380..780 {
+        let meter_wavelength = wavelength as f64 / 1e9; 
+        let irradiance = blackbody_radiance_at_frequency(meter_wavelength as f64, temperature as f64);
+        let stimulus = CIE_stimulus(wavelength as f64);
+
+        X += stimulus.0 * irradiance;
+        Y += stimulus.1 * irradiance;
+        Z += stimulus.2 * irradiance;
+    }
+
+    let normalize = 1.0 / Y;
+
+    return (X * normalize, Y * normalize, Z * normalize);
+}
+
+fn blackbody_radiance_at_frequency(wavelength: f64, temperature: f64) -> f64
+{
+    const PLANCK_CONSTANT: f64 = 6.62607015e-34;
+    const BOLTZMANN_CONSTANT: f64 = 1.380649e-23;
+    const C: f64 = 299792458.0;
+
+    let left = (C.powi(2) * PLANCK_CONSTANT * 2.0 ) / wavelength.powi(5);
+    let exponent = (PLANCK_CONSTANT * C) / (wavelength * BOLTZMANN_CONSTANT * temperature);
+    let right = 1.0 / (exponent.exp() - 1.0);
+
+    return (left * right);
+}
+
+fn CIE_to_sRGB(colors: ColorTriple) -> ColorTriple {
+    let x = colors.0;
+    let y = colors.1;
+    let z = colors.2;
+
+    let red_linear = (3.2406 * x) + (-1.5372 * x) + (-0.4986 * x);
+    let green_linear = (-0.9689 * y) + (1.8758 * y) + (0.0415 * y);
+    let blue_linear = (0.0557 * z) + (-0.2042 * z) + (1.0570 * z);
+
+    return (
+        delinearize_sRGB(red_linear),
+        delinearize_sRGB(green_linear),
+        delinearize_sRGB(blue_linear),
+    );
+}
+
+fn delinearize_sRGB(color: f64) -> f64 {
+    if color <= 0.0031308 {
+        return color * 12.92;
+    } else {
+        return (1.055 * color.powf(1.0 / 2.4)) - 0.055;
+    }
+}
+
+fn CIE_stimulus(wavelength: f64) -> ColorTriple {
+    let x_bar = (1.056 * CIE_gaussian(wavelength, 599.8, 37.9, 31.0))
+        + (0.362 * CIE_gaussian(wavelength, 442.0, 16.0, 26.7))
+        + (-0.065 * CIE_gaussian(wavelength, 501.1, 20.4, 26.2));
+
+    let y_bar = (0.821 * CIE_gaussian(wavelength, 568.8, 46.9, 40.5))
+        + (0.286 * CIE_gaussian(wavelength, 530.9, 16.3, 31.1));
+
+    let z_bar = (1.217 * CIE_gaussian(wavelength, 437.0, 11.8, 36.0))
+        + (0.681 * CIE_gaussian(wavelength, 459.0, 26.0, 13.8));
+
+    return (x_bar, y_bar, z_bar);
+}
+
+fn CIE_gaussian(wavelength: f64, peak: f64, left: f64, right: f64) -> f64 {
+    let identical = -0.5 * (wavelength - peak).powi(2);
+    if wavelength < peak {
+        return (identical / left.powi(2)).exp();
+    } else {
+        return (identical / right.powi(2)).exp();
+    }
+}
