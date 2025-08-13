31d7de898d88f53510aa379ed179dcc8e7721a0b
Grant Jennings <gajbooks@gmail.com>
2025-08-12 21:14:05 -0500

    Standalone Update?

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { provideHttpClient, withFetch } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 
-export const appConfig: ApplicationConfig = {  providers: [    provideHttpClient(withFetch()),  ]};
+export const appConfig: ApplicationConfig = { providers: [provideHttpClient(withFetch()),] };
 
 
-    
