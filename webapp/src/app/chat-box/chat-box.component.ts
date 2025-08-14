bf7a88e77e92ea3e48b51dda2b3602090d200082
Grant Jennings <gajbooks@gmail.com>
2025-08-14 18:04:52 -0500

    Basic Chat Box Impl

--- webapp/src/app/chat-box/chat-box.component.ts
+++ webapp/src/app/chat-box/chat-box.component.ts
@@ -1,27 +1,46 @@
 import { Component, inject } from "@angular/core";
 import { ChatService } from "../services/chat.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+
+const MAX_MESSAGE_COUNT: number = 20;
 
 @Component({
 providers: [ChatService],
+  imports: [ReactiveFormsModule],
   selector: 'chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.less']
 })
 export class ChatBoxComponent {
-    private chat_service = inject(ChatService);
+    private chatService = inject(ChatService);
+
+    chatInputBox = new FormControl('');
+
+    chatSubmitGroup = new FormGroup([this.chatInputBox]);
+
+    messages: {name: string, message: string}[] = [];
 
   async ngOnInit() {
     let self = this;
-    setInterval(() => {
-        self.sendTime();
-    }, 5000);
 
-    (await this.chat_service.subscribeToChat()).subscribe((message) => {
+    (await this.chatService.subscribeToChat()).subscribe((message) => {
+        self.receive(message.player_name, message.message);
         console.log("Received: %s %s", message.player_name, message.message);
     })
   }
 
-  async sendTime() {
-    await this.chat_service.sendMessage(new Date(Date.now()).toString());
+  async send() {
+    let message = this.chatInputBox.value;
+    if (message != null) {
+        await this.chatService.sendMessage(message);
+    }
+    this.chatInputBox.setValue(null);
+  }
+
+  receive(name: string | null, message: string) {
+    this.messages.push({name: name ?? "???", message});
+    if (this.messages.length > MAX_MESSAGE_COUNT) {
+        this.messages.shift();
+    }
   }
 }
